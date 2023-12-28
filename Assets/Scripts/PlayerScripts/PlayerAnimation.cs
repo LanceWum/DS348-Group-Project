ca9503e9af4a28ad04c1c28bdf34449638bb8c93
Lance Wu <50177683+LanceWum@users.noreply.github.com>
2023-12-29 01:24:36 +0800

    Added Player Movement Animation
    1. Added player animation for movement and jump.

---
+++ Assets/Scripts/PlayerScripts/PlayerAnimation.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private PhysicsCheck _physicsCheck;
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        _physicsCheck = GetComponent<PhysicsCheck>();
+    }
+
+    private void Update()
+    {
+        SetAnimations();
+    }
+
+    public void SetAnimations()
+    {
+        anim.SetFloat("velocityX", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("velocityY", rb.velocity.y);
+        anim.SetBool("isGround", _physicsCheck.isGround);
+    }
+}
